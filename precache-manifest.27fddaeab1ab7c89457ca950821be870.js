44eb04daa533feb0caa00973c7082d086a0ac7f3
DaNuwa <yhan7788@gmail.com>
2019-12-12 22:10:41 +0530

    Updates

---
+++ precache-manifest.27fddaeab1ab7c89457ca950821be870.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a391615bbdea0efde534fbd5c8aaf982",
+    "url": "/Christmas-Todo/index.html"
+  },
+  {
+    "revision": "0bb20e9e94f92b858a1e",
+    "url": "/Christmas-Todo/static/css/main.8e8a2d5c.chunk.css"
+  },
+  {
+    "revision": "45b71a4e2dc072d89d4e",
+    "url": "/Christmas-Todo/static/js/2.3c9d194c.chunk.js"
+  },
+  {
+    "revision": "0bb20e9e94f92b858a1e",
+    "url": "/Christmas-Todo/static/js/main.d9831cae.chunk.js"
+  },
+  {
+    "revision": "5fd49ac7c040cfacde82",
+    "url": "/Christmas-Todo/static/js/runtime-main.cc96cc5f.js"
+  }
+]);
